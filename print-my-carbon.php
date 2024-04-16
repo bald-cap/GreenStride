29ce518578591474d3e5e6c50ef58a89826add1b
NYANYUIE Michael <mnyanyuie@gmail.com>
2024-04-16 21:09:38 +0200

    Update print-my-carbon.php

--- print-my-carbon.php
+++ print-my-carbon.php
@@ -4,21 +4,5 @@
     $connection = mysqli_connect(ID, PASSWORD, DB, SERVER);
 
     if($connection){
-        $actors = mysqli_query(
-            $connection,
-            "SELECT NumInd, Role
-            FROM L2_Acteurs"
-        );
-
-        foreach($actors as $key => $value){
-            if ($key == 'NumInd'){
-                foreach($value as $id){
-                    echo "<p> $id </p>";
-                }
-            }elseif($key == 'Role'){
-                foreach($value as $role){
-                    echo "<p> $role </p>";
-                }
-            }   
-        }
+        //Store info in DB
     }
